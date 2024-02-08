62f89eed682c07414718b8d7364a6756cfb6784b
Sushma Tene <150417347+Sushmatech@users.noreply.github.com>
2024-02-08 10:10:08 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sushbucket-hotstar" # Replace with your actual S3 bucket name
+    bucket = "sushbucket-1997" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
